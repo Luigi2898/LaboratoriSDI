12bc3339392922d8767f2577602a3e8aa414b70c
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2019-11-29 12:40:35 +0100

    Control Unit

--- Lab2/CU.vhd
+++ Lab2/CU.vhd
@@ -4,15 +4,25 @@ USE IEEE.NUMERIC_STD.ALL;
 
 entity CU is
   port (
-    clock     : in std_logic;
-    rst       : in std_logic;
-    baud_end  : in std_logic;
-    shift_end : in std_logic;
-    dp_rst    : out std_logic;
-    baud_cnt  : out std_logic;
-    shift_cnt : out std_logic;
-    shift_en  : out std_logic;
-    load_en   : out std_logic;
-    read_en   : out std_logic
+    clock      : in std_logic;
+    rst        : in std_logic;
+    baud_end   : in std_logic;
+    shift_end  : in std_logic;
+    data_valid : in std_logic;
+    dp_rst     : out std_logic;
+    baud_cnt   : out std_logic;
+    shift_cnt  : out std_logic;
+    shift_en   : out std_logic;
+    load_en    : out std_logic;
+    read_en    : out std_logic;
+    txready    : out std_logic
   );
 end entity;
+
+architecture Behavioural of CU is
+
+  type state_type is (RESET, IDLE, TRANSMIT, BUSY);
+
+begin
+
+end architecture;
