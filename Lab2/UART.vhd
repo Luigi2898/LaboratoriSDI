aac95e7d04f477c755b83b45064cc85df75ec93c
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2019-12-03 17:59:28 +0100

    Update UART.vhd

--- Lab2/UART.vhd
+++ Lab2/UART.vhd
@@ -1,4 +1,27 @@
 library ieee;
 use ieee.std_logic_vector.all;
 
-entity 
+entity UART is
+  
+  port(
+    data_in  : in std_logic_vector (7 downto 0);
+    tx       : out std_logic;
+    tx_ready : out std_logic;
+    wr       : in std_logic;
+    
+    data_out : out std_logic_vector (7 downto 0);
+    rx       : in std_logic;
+    rd       : in std_logic;
+    dav      : out std_logic;
+    
+    clock    : in std_logic;
+    resetN   : in std_logic;
+    )
+  
+end entity;
+ 
+architecture Behavioural of UART is
+   
+  
+  
+end architecture;
