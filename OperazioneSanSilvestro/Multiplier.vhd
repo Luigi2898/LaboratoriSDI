508c437c15a32e3ef789b0ad0be16068dbb6fd73
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2019-12-14 10:18:45 +0100

    Update Multiplier

--- OperazioneSanSilvestro/Multiplier.vhd
+++ OperazioneSanSilvestro/Multiplier.vhd
@@ -2,14 +2,18 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
-entity multiplier is
+entity Multiplier is
+  generic(Nbit : integer := 8);
   port (
-  clock
+    In1   : in std_logic_vector(Nbit - 1 downto 0);
+    In2   : in std_logic_vector(Nbit - 1 downto 0);
+    Mult2 : in std_logic; -- Se '1' multiplica per 2, altrimenti moltiplica i due ingressi
+    Res   : out std_logic_vector(2 * Nbit - 2 downto 0)
   );
 end entity;
 
-architecture  of multiplier is
+architecture behavioural of Multiplier is
 
 begin
-
+  
 end architecture;
