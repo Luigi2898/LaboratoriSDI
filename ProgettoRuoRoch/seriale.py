10ed79b872397ff5d48469df97a0689f468be2cd
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2020-01-11 18:17:42 +0100

    Update seriale.py

--- ProgettoRuoRoch/seriale.py
+++ ProgettoRuoRoch/seriale.py
@@ -1,5 +1,11 @@
 import serial
 from serial import Serial
+out_file = open("test.txt","w")
 porta = serial.Serial("COM5", 9600, 8, "N", 1, timeout=1)
 porta.write(b'test')
+while porta.is_open:
+    out_file.write(porta.read().decode("utf-8"))
 porta.close()
+out_file.close()
+if porta.is_closed:
+    print("Ho chiuso file e com :)")
