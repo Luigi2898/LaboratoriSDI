64d575928802cb97ceec68cfafc4f06103a9d645
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2020-01-12 22:49:51 +0100

    Update seriale.py

--- ProgettoRuoRoch/seriale/seriale.py
+++ ProgettoRuoRoch/seriale/seriale.py
@@ -2,6 +2,7 @@ import serial
 from serial import Serial
 out_file = open("out1.txt","w")
 porta = serial.Serial("COM5", 9600, 8, "N", 1, timeout = 1)
+print("Apro la seriale")
 i = 1
 while i:
     try:
