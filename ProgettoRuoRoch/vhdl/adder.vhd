595cb351c3e1b24318c83ef33fc498ed589c8bb0
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2020-01-13 23:51:34 +0100

    Update adder.vhd

--- ProgettoRuoRoch/vhdl/adder.vhd
+++ ProgettoRuoRoch/vhdl/adder.vhd
@@ -1,11 +1,12 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
+  use ieee.std_logic_unsigned.all;
 
 entity adder is
   port (
     in1, in2 : in std_logic_vector(24 downto 0);
-    res      : out std_logic_vector(25 downto 0)
+    res      : out std_logic_vector(24 downto 0)
   );
 end entity;
 
@@ -13,6 +14,6 @@ architecture arch of adder is
 
 begin
 
-  res <= to_intger(signed(in1)) + to_intger(signed(in2));
+  res <= in1 + in2;
 
 end architecture;
