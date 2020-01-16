e67a6b1a3c8fe0236f12e358306bc9fbac547e4e
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2020-01-16 17:13:36 +0100

    Update seriale.py

--- ProgettoRuoRoch/seriale/seriale.py
+++ ProgettoRuoRoch/seriale/seriale.py
@@ -1,13 +1,11 @@
 import serial
 from serial import Serial
 out_file = open("out1.txt","w")
-<<<<<<< HEAD
 porta = serial.Serial("COM5", 9600, 8, "N", 1)
 i = 1
 while i:
     try:
         out_file.write(porta.read().decode("utf-8"))
-=======
 porta = serial.Serial("COM5", 9600, 8, "N", 1, timeout = 1)
 print("Apro la seriale")
 i = 1
@@ -17,7 +15,6 @@ while i:
         out_file.write(r)
         if r != "":
             print(r)
->>>>>>> d30787381ca5218a4775b79ab4a2516091391e2d
     except KeyboardInterrupt:
         print("Esco dal ciclo ;)")
         i = 0
