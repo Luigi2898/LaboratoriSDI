835f90091f578885001c23b8288f7f4fbeb6bd8f
Luigi <luigi.giuffrida.98@gmail.com>
2020-02-10 22:28:29 +0100

    Create Estimator.vhd

---
+++ ProgettoRuoRoch/vhdl/Direction_estimator.vhd/Estimator.vhd
@@ -0,0 +1,15 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity Estimator is
+  port (
+
+  );
+end entity;
+
+architecture arch of Estimator is
+
+begin
+
+end architecture;
