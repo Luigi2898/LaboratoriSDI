f87616986a82365f86cefe1692321a3d92921440
Luigi <luigi.giuffrida.98@gmail.com>
2020-02-11 10:18:01 +0100

    Update file.py

--- ProgettoRuoRoch/Matlab/file.py
+++ ProgettoRuoRoch/Matlab/file.py
@@ -4,8 +4,8 @@ file_in_dx = open("PDMd.txt", "r").readlines()
 
 i = 0
 while i < len(file_in_dx):
-    file_out.write(file_in_sx[i])
     file_out.write(file_in_dx[i])
+    file_out.write(file_in_sx[i])
     i = i + 1
 
 file_out.close()
