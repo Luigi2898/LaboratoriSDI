2aff2271cc494c56c801a78d70badfeb71d3bea5
Luigi <luigi.giuffrida.98@gmail.com>
2020-02-26 20:19:19 +0100

    Create prova.vhd

---
+++ ProgettoRuoRoch/vhdl/MIC_FRONT_END/prova.vhd
@@ -0,0 +1,15 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity pippo is
+  port (
+  clock
+  );
+end entity;
+
+architecture beh of pippo is
+
+begin
+
+end architecture;
